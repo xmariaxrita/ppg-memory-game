fffa3cee799121dfb6f9b7e72726d6842d25bbba
xmariaxrita <xmariaxrita4@gmail.com>
2024-12-09 07:36:15 -0300

    Inicio da criação da tela de login

--- setup/urls.py
+++ setup/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('login/', login)
 ]
